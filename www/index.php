013066d61439b1e39aa8499a71cff5186478ab23
Joelle <castelli.jo@gmail.com>
2021-07-21 20:47:57 +0200

    working redis

--- www/index.php
+++ www/index.php
@@ -6,7 +6,24 @@ $sth->execute();
 $name = $sth->fetchColumn();
 
 
-echo "<h1 style='color: deeppink'>Hello $name</h1>"
+echo "<h1 style='color: deeppink'>Hello $name</h1>";
 
+$redis = new Redis();
+$redis->connect("redis", 6379);
+echo "<div>Le serveur Redis est lancé : " . $redis->ping()."</div>";
+
+
+$redis->lpush("contributors", "Coraline");
+$redis->lpush("contributors", "Joëlle");
+$contributors = $redis->lrange("contributors", 0 ,1);
+
+echo "<br><div>";
+echo "Contributeurs :";
+echo "<ul>";
+foreach ($contributors as $contributor) {
+    echo "<li>".$contributor;
+}
+echo "</ul>";
+echo "</div>";
 
 ?>
