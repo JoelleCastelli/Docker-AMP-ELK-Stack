175ddfe27593899c5eaf3c25baa642ac42ac1f64
Joelle <castelli.jo@gmail.com>
2021-07-21 19:35:20 +0200

    get name from database

--- www/index.php
+++ www/index.php
@@ -1,1 +1,12 @@
-<?php echo "<h1 style='color: deeppink'>Hello World</h1>" ?>
+<?php
+
+$pdo = new \PDO("mysql:host=database; dbname=docker; port=3306; charset=UTF8", "root", "password");
+$sth = $pdo->prepare("SELECT name FROM users where id = 1");
+$sth->execute();
+$name = $sth->fetchColumn();
+
+
+echo "<h1 style='color: deeppink'>Hello $name</h1>"
+
+
+?>
